d28586a4b0d4d0373b747f422dd23606bbeae915
Yordan VeselinoV <60753194+ntesdaf@users.noreply.github.com>
2020-02-22 02:23:22 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,105 @@
+package job;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String error = "Invalid number!";
+        String message = "Enter an odd number bigger than 2 and smaller than 10 000:";
+        boolean isInt = false;
+        int n;
+        System.out.println(message);
+        String number = scanner.nextLine();
+        String num;
+        boolean flag = true;
+        while (flag) {
+            String[] numberArray = number.split("");
+            List<String> passList = new ArrayList<>();
+            for (int i = 0; i < numberArray.length; i++) {
+                int symbol = numberArray[i].charAt(0);
+                if (symbol > 47 && symbol < 58) {
+                    passList.add(numberArray[i]);
+                }
+            }
+            if (passList.size() != numberArray.length) {
+                System.out.println(error);
+                System.out.println(message);
+                number = scanner.nextLine();
+            } else {
+                isInt = true;
+            }
+            if (isInt) {
+                num = number;
+                n = Integer.parseInt(num);
+                if (n % 2 != 0 && n > 2 && n < 10000) {
+                    String dash = "-";
+                    String star = "*";
+                    for (int i = 0; i < n + 1; i++) {
+                        for (int j = 0; j < n * 10; j++) {
+                            if (i < (n + 1) / 2) {
+                                if (j < n - i) {
+                                    System.out.print(dash);
+                                } else if (j < (2 * n) + i) {
+                                    System.out.print(star);
+                                } else if (j < (3 * n) - i) {
+                                    System.out.print(dash);
+                                } else if (j < (4 * n) + i) {
+                                    System.out.print(star);
+                                } else if (j < (6 * n) - i) {
+                                    System.out.print(dash);
+                                } else if (j < (7 * n) + i) {
+                                    System.out.print(star);
+                                } else if (j < (8 * n) - i) {
+                                    System.out.print(dash);
+                                } else if (j < (9 * n) + i) {
+                                    System.out.print(star);
+                                } else if (j < (10 * n) + i) {
+                                    System.out.print(dash);
+                                }
+                            } else {
+                                if (j < (n - i)) {
+                                    System.out.print(dash);
+                                } else if (j < (2 * n) - i) {
+                                    System.out.print(star);
+                                } else if (j < (2 * n) - (n - i)) {
+                                    System.out.print(dash);
+                                } else if (j < (3 * n) + (n - i)) {
+                                    System.out.print(star);
+                                } else if (j < (4 * n) - (n - i)) {
+                                    System.out.print(dash);
+                                } else if (j < (5 * n) - (n - i)) {
+                                    System.out.print(star);
+                                } else if (j < (6 * n) - i) {
+                                    System.out.print(dash);
+                                } else if (j < (7 * n) - i) {
+                                    System.out.print(star);
+                                } else if (j < (7 * n) - (n - i)) {
+                                    System.out.print(dash);
+                                } else if (j < (9 * n) - i) {
+                                    System.out.print(star);
+                                } else if (j < (9 * n) - (n - i)) {
+                                    System.out.print(dash);
+                                } else if (j < (10 * n) - (n - i)) {
+                                    System.out.print(star);
+                                } else if (j < (10 * n)) {
+                                    System.out.print(dash);
+                                }
+                            }
+                        }
+                        if (i < n) {
+                            System.out.println();
+                        }
+                    }
+                    flag = false;
+                } else {
+                    System.out.println(error);
+                    System.out.println(message);
+                    number = scanner.nextLine();
+                }
+            }
+        }
+    }
+}
